a65dcf52561be1b8b0196f6b0efeb7c4c4628b1f
Andrei <andreiing@yahoo.com>
2026-08-19 16:28:38 +0300

    Added Items.

---
+++ src/main/java/com/test/controller/ItemController.java
@@ -0,0 +1,37 @@
+package com.test.controller;
+
+import com.test.model.Item;
+import com.test.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/items")
+    public List<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
+
+    @GetMapping("/item/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+        return itemService.getItemById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/item")
+    public Item createItem(@RequestBody Item item) {
+        return itemService.createItem(item);
+    }
+}
--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
